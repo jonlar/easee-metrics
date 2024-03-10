f3e6706ff7f899cba94b5fe6cf39f30b06a823d3
Jonas Larsson <jonas@larsson.eu.com>
2024-03-10 11:45:24 +0100

    Rewrite auth logic

--- src/easee/index.ts
+++ src/easee/index.ts
@@ -12,48 +12,76 @@ export class Easee {
   private userId: number | undefined;
   private credentials: EaseeProps;
   private refreshTimer: NodeJS.Timeout | undefined;
-  private token: string | null;
+  private token: string | undefined;
 
   constructor(props: EaseeProps) {
     if (!props.userName || !props.password) throw Error('Set EASEE_USERNAME and EASEE_PASSWORD');
     this.credentials = props;
-    this.token = null;
   }
 
-  async _handleCredentials(accessToken: string | null, refreshToken: string | null, expiresIn: Duration | null) {
-    if (!accessToken || !refreshToken || !expiresIn) {
-      throw new Error('Failed to login');
+  private async updateToken(token?: string) {
+    this.token = token;
+    const baerer = `Bearer ${token}`;
+    log.debug(baerer);
+    if (token) {
+      easeeApi.axios.defaults.headers.common = {
+        Authorization: baerer,
+      };
+    } else {
+      delete easeeApi.axios.defaults.headers.common.Authorization;
+    }
+  }
+
+  private async handleRefresh(options: { refreshToken: string; expiresIn?: Duration }) {
+    const refreshAt = options.expiresIn ? options.expiresIn.sub(Duration.second(60)) : undefined;
+    if (!refreshAt) {
+      log.error('Cannot schedule session refresh, expiry time not set');
+      return;
     }
 
-    this.token = accessToken;
-    easeeApi.axios.defaults.headers.common = {
-      Authorization: `Bearer ${accessToken}`,
-    };
-
-    const refreshAt = expiresIn.sub(Duration.second(60));
     this.refreshTimer = setTimeout(async () => {
+      if (!this.token) {
+        log.error('Cannot refresh session, no token set');
+        this.logout();
+        await this.login();
+        return;
+      }
+
       const data = await easeeApi.postApiaccountsrefresh_token({
-        accessToken,
-        refreshToken,
+        accessToken: this.token,
+        refreshToken: options.refreshToken,
       });
 
-      this._handleCredentials(
-        data.accessToken ?? null,
-        data.refreshToken ?? null,
-        data.expiresIn ? Duration.second(data.expiresIn) : null,
-      );
-    }, refreshAt.milliseconds);
+      if (!data.accessToken) {
+        log.error('Session refresh failed');
+        this.logout();
+        await this.login();
+        return;
+      }
+      this.updateToken(data.accessToken);
+      log.info(`Session refreshed, expires in ´${data.expiresIn} seconds`);
 
-    log.info(`Session refreshed, expires in ´${expiresIn}`);
+      if (data.refreshToken && data.expiresIn) {
+        await this.handleRefresh({
+          refreshToken: data.refreshToken,
+          expiresIn: data.expiresIn ? Duration.second(data.expiresIn) : undefined,
+        });
+      }
+    }, refreshAt.milliseconds);
   }
 
   async login() {
     const data = await easeeApi.postApiaccountslogin(this.credentials);
-    this._handleCredentials(
-      data.accessToken ?? null,
-      data.refreshToken ?? null,
-      data.expiresIn ? Duration.second(data.expiresIn) : null,
-    );
+    if (!data.accessToken) {
+      throw new Error('Login failed');
+    }
+    this.updateToken(data.accessToken ?? undefined);
+    if (data.refreshToken && data.expiresIn) {
+      await this.handleRefresh({
+        refreshToken: data.refreshToken,
+        expiresIn: Duration.second(data.expiresIn),
+      });
+    }
 
     const profileInfo = await easeeApi.getApiaccountsprofile();
     this.userId = profileInfo.userId;
@@ -65,6 +93,7 @@ export class Easee {
     if (this.refreshTimer) {
       clearTimeout(this.refreshTimer);
     }
+    this.token = undefined;
     log.info('Logged out');
   }
 
